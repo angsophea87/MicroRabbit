3adddb449d94b771e78547e8350c54ecdd164816
sophea ang <angsophea@outlook.com>
2021-05-17 16:00:21 +0700

    Implemented Interfaces and classes for Domain.Core

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,22 @@
+﻿using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+        void Publish<T>(T @event) where T : Event;
+        void Subscribe<T, TH>() where T : Event where TH : IEventHandler<T>;
+
+        /*Task SendCommand<TCommand>(TCommand command) where TCommand : Command;
+        void Publish<TEvent>(TEvent @event) where TEvent : Event;
+        void Subscribe<TEvent, TEventHandler>() where TEvent : Event where TEventHandler : IEventHandler<TEvent>;*/
+
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Event
+    {
+        protected Event()
+        {
+            Timestamp = DateTime.Now;
+            /*Timestamp = DateTime.UtcNow;*/
+        }
+
+        public DateTime Timestamp { get; protected set; }
+    }
+}
