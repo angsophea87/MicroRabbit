5daa8a081948fcce4c7b67fc2fea7956181bc170
sophea ang <angsophea@outlook.com>
2021-05-18 07:59:44 +0700

    Partially completed Banking Microservice

---
+++ MicroRabbit/MicroRabbit.Banking.Data/Context/BankingDbContext.cs
@@ -0,0 +1,28 @@
+﻿using MicroRabbit.Banking.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Banking.Data.Context
+{
+    public class BankingDbContext : DbContext
+    {
+        public BankingDbContext(DbContextOptions options) : base(options)
+        {
+#if DEBUG
+            Database.EnsureCreated();
+#endif
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+
+/*        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Account>().Property(x => x.AccountBalance).HasColumnType("decimal(18,2)");
+        }*/
+    }
+}
